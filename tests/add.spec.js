9be6588eee5f41dc0ab5447167170173baa8e280
aythp <ayznk9@gmail.com>
2025-01-19 02:19:55 +0000

    solved lab

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,15 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+      
+      it("should return undefined if any of the arguments is not a number", () => {
+        expect(add(1, "2")).toEqual(undefined);
+        expect(add("3", 4)).toEqual(undefined);
+        expect(add(5, true)).toEqual(undefined);
+        expect(add(6, null)).toEqual(undefined);
+        expect(add(null, null)).toEqual(undefined);
+      });
+
     });
   });
   
--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -1,14 +1,32 @@
-// UNCOMMENT THE CODE BELOW TO START
-
-/*
-
 describe("Iteration 3", () => {
     describe("Function - calculateArea", () => {
         
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+        it("should be defined", () => {
+            expect(calculateArea).toBeDefined();
+        });
+
+        it("should take two arguments", () => {
+            expect(calculateArea.length).toBe(2)
+        });
+
+        it("should return the rectangle area by multiplying the two numbers", () => {
+            expect(calculateArea(10, 2)).toEqual(20);
+        });
+
+        it("should return undefined if any of the arguments is not provided", () => {
+            expect(calculateArea(1,)).toEqual(undefined);
+            expect(calculateArea()).toEqual(undefined);
+            expect(calculateArea(undefined, 1)).toEqual(undefined);
+        });
+
+        
+        it("should return undefined if any of the arguments is not a number", () => {
+            expect(calculateArea(1, "2")).toEqual(undefined);
+        expect(calculateArea("3", 4)).toEqual(undefined);
+        expect(calculateArea(5, true)).toEqual(undefined);
+        expect(calculateArea(6, null)).toEqual(undefined);
+        expect(calculateArea(null, null)).toEqual(undefined);
+        });
 
     })    
 })
-
-*/
